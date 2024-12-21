9493c4210dc857f8d535569da5b0321d287ad765
Salvador Salcedo <salsalcedo4321@gmail.com>
2024-12-21 10:39:33 -0800

    Refactor run.sh to enable terminal commands for service execution
    - Reintroduced terminal command execution for starting services.
    - Removed commented-out legacy code for clarity and maintainability.
    - Ensured proper activation of terminal and execution of scripts in new tabs.

--- run.sh
+++ run.sh
@@ -9,18 +9,17 @@ osascript -e "tell application \"Terminal\" to do script \"${service1}\""
 osascript -e "tell application \"Terminal\" to do script \"${service2}\""
 
 # Start server in Linux
-
-# osascript -e 'tell application "Terminal" to activate' \
-#     -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
-#     -e 'tell application "Terminal" to do script "echo hello" in selected tab of the front window'
-# osascript -e "
-#       tell application \"Terminal\"
-#         activate
-#         tell application \"System Events\" to keystroke \"t\" using command down
-#         repeat while contents of selected tab of window 1 starts with linefeed
-#           delay 0.01
-#         end repeat
-#         do script \"${service1}\" in window 1
-#       end tell
-#     "
-#./chainlink node start && python /iris-external-adapter app.py
+osascript -e 'tell application "Terminal" to activate' \
+    -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' \
+    -e 'tell application "Terminal" to do script "echo hello" in selected tab of the front window'
+osascript -e "
+      tell application \"Terminal\"
+        activate
+        tell application \"System Events\" to keystroke \"t\" using command down
+        repeat while contents of selected tab of window 1 starts with linefeed
+          delay 0.01
+        end repeat
+        do script \"${service1}\" in window 1
+      end tell
+    "
+./chainlink node start && python /iris-external-adapter app.py
